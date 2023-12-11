aa97b73017923a9e51d802ca7d9df5986ae66f3c
Alberto <74522184+albim360@users.noreply.github.com>
2023-12-11 16:24:16 +0100

    FEAT post added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
-import { useState } from 'react'
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import HomePage from './components/HomePage'
-import BlogPage from './components/BlogPage'
-import PostPage from './components/PostPage'
-import Navbar from './components/Navbar'
-import './App.css'
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import BlogPage from './components/BlogPage';
+import PostPage from './components/PostPage';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
   return (
     <Router>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path='/' exact component={HomePage} />
-        <Route path='/blog' component={BlogPage} />
-        <Route path='/post' component={PostPage} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/post/*" element={<PostRoutes />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+const PostRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<PostPage />} />
+      <Route path=":id" element={<SinglePostPage />} />
+    </Routes>
+  );
+};
+
+const SinglePostPage = () => {
+  const { id } = useParams();
+
+  const apiUrl = `https://apigenerator.dronahq.com/api/u0ssVH5U/post/${id}`;
+
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => setPost(data))
+      .catch(error => console.error('Errore durante il recupero dei dettagli del post:', error));
+  }, [apiUrl]);
+
+  if (!post) {
+    return <div>Caricamento...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      <p>Tag: {post.tag.join(', ')}</p>
+      <img src={post.image} alt={post.title} />
+    </div>
+  );
+};
+
+export default App;
--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,11 +1,30 @@
-import React from'react';
+import React, { useState, useEffect } from 'react';
 
 const BlogPage = () => {
-    return (
-        <div>
-            <h1>BlogPage</h1>
-        </div>
-    );
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://apigenerator.dronahq.com/api/u0ssVH5U/post')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Errore durante il recupero dei dati:', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista dei post</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <a href={`/post/${post.id}`}>
+              <h3>{post.title}</h3>
+            </a>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default BlogPage;
